d08f0aca0622d10b4d713e3536876151561e350d
Urs <urs.gompper@gmail.com>
2020-03-14 21:58:45 +0100

    grafiken beschriftet

--- growth_rate.py
+++ growth_rate.py
@@ -6,7 +6,6 @@ import numpy as np
 data = requests.get('https://de.wikipedia.org/wiki/COVID-19-F%C3%A4lle_in_Deutschland#Statistik')
 soup = BeautifulSoup(data.text, 'html.parser')
 
-
 data = []
 number = []
 start = 0
@@ -23,14 +22,27 @@ for tr in soup.find_all('tr'):
                     for i in range(0,len(th.text)):
                         if th.text[i].isnumeric():
                             tmp = tmp + th.text[i]
+                        else:
+                            break
                     number.append(tmp)
-                    print(tmp)
- 
     if start == 1:
         break
 
 numberArr = np.array([float(i) for i in number])        
-plt.plot(numberArr.T)     
+
+
+growth = np.empty(len(numberArr)-1)
+for i in range(0,len(numberArr)-1):
+    growth[i] = numberArr[i+1]/numberArr[i]
+
+plt.figure(1)
+plt.plot(numberArr,'bo-')     
+plt.title('Wachstum pro Tag')
+plt.show()
+
+plt.plot(growth,'bo-')
+plt.title('Wachstumsfaktor')
 plt.show()
 
 
+    
