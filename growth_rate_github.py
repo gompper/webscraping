45f5b3795afa83dd4ab0731bc3582cf2f2cf95e4
Urs <urs.gompper@gmail.com>
2020-03-25 11:59:15 +0100

    URL changed + USA accumulated + plots adjusted

--- growth_rate_github.py
+++ growth_rate_github.py
@@ -9,16 +9,27 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 
-url = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv'
+#url = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv'
+url = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv'
 
 data = pd.read_csv(url)
 data_np = data.to_numpy()
+
+# Accumulate USA 
+for i in range(0,len(data_np)):
+    if data_np[i][1] == 'US':
+        for j in range(4,len(data_np[0])):
+            data_np[98][j] = data_np[98][j] + data_np[i][j]
+
 
 daily = np.empty([len(data),len(data_np[11])-6])
 for j in range(0,len(data)):
     for i in range(6,len(data_np[11])):
         daily[j][i-6] = data_np[j][i]
         
+
+        
+
 
 daily_growth = np.empty([len(data),len(daily[0])-1])    
 for j in range(0,len(daily)-1):
@@ -37,12 +48,17 @@ for j in range(0,len(daily_growth)):
         
         
 plt.figure(1)
-plt.subplot(211)
+plt.subplots_adjust(hspace=0.8)
+plt.subplot(311)
 plt.axis([0,len(growth[0]),0,5])
-plt.plot(growth[11],'o-')
+plt.plot(growth[120],'o-')
 plt.title('Wachstumsfaktor Covid-19 Deutschland')
-plt.subplot(212)
+plt.subplot(312)
 plt.axis([0,len(growth[0]),0,5])
-plt.plot(growth[35],'o-')
+plt.plot(growth[28],'o-')
 plt.title('Wachstumsfaktor Covid-19 Brasilien')
+plt.subplot(313)
+plt.axis([0,len(growth[0]),0,5])
+plt.plot(growth[98],'o-')
+plt.title('Wachstumsfaktor Covid-19 USA')
 plt.show()
